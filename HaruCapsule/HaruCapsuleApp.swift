4033e81dcec890bd86891a400837b4ff02b005dc
Dorii0513 <bobodorii0513@gmail.com>
2024-03-22 11:03:02 +0900

    Initial Commit

---
+++ HaruCapsule/HaruCapsuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaruCapsuleApp.swift
+//  HaruCapsule
+//
+//  Created by 김예림 on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct HaruCapsuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
